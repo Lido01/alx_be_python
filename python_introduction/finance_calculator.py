0bc3ff68cd25ef39665a4a5765d29db8bbdb28a9
Lidetu Tesfaye <lidosinafanis@gmail.com>
2024-12-15 00:26:34 -0800

    Update finance_calculator code error

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,8 @@
 month_income = int(input("Enter your monthly income:"))
 month_expenses = int(input("Enter your total monthly expenses:"))
-monthly_saving = month_income - month_expenses
-Projected_saving = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
-print("Your monthly saving are", f"${monthly_saving}")
+Monthly_saving = month_income - month_expenses
+Projected_saving = Monthly_saving * 12 + (Monthly_saving * 12 * 0.05)
+Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05))
+print("Your monthly saving are", f"${Monthly_saving}")
 print("Projected saving after one year, with interest, is:",f"${Projected_saving}")
+
