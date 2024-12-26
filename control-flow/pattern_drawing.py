7714ab5b70f7e56f82f933d5bdec17524bb4cd0b
Lidetu Tesfaye <lidosinafanis@gmail.com>
2024-12-26 15:36:39 -0800

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,4 +1,4 @@
+# Prompt user for the size of pattern
 n = int(input("Enter the size of the pattern:"))
-while :
-    for 
-        print("*", end="")
+for i in range(1, 11):
+    print(n, "*" ,i,"=",n*i)
