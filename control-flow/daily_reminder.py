1989e4c4dc293ea363df46fa2f822c9752736552
Lidetu Tesfaye <lidosinafanis@gmail.com>
2024-12-27 18:16:41 -0800

    Create daily_reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+task = input("Enter your task: ")
+priority = input("Enter your task's priority (high/medium/low): ")
+time_bound = input("Is it time bound? (yes/no):").lower()
+
+match priority:
+    case "high":
+        level = "high"
+    case "medium":
+        level = "medium"
+    case "low":
+        level = "low"
+if time_bound == "yes":
+    print(f"Reminder: { task} is a {level} priority task that requires immediate attention today!" )
+else:
+    print(f"Note: { task} is a {level} priority task. Consider completing it when you have free time.")
+
