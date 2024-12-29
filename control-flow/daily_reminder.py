4deb12f84245f4699f246855ec1f262fd885c32f
Lidetu Tesfaye <lidosinafanis@gmail.com>
2024-12-28 22:04:07 -0800

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task=input("Enter your task: ")
-priority=input("Enter your task's priority (high/medium/low): ").lower()
-time_bound=input("Is it time bound? (yes/no):").lower()
+priority=input("Priority (high/medium/low): ").lower()
+time_bound=input("Is it time bound? (yes/no): ").lower()
 match priority:
     case "high":
         level = "high"
