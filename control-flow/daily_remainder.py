7a419cb527ffc9ffc70a1bdbdcdb039b99419ccf
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-10 02:16:20 -0800

    Update daily_remainder.py

--- control-flow/daily_remainder.py
+++ control-flow/daily_remainder.py
@@ -1,23 +1,23 @@
 task = input("Enter your task: ")
 priority = input("Enter your task's priority (high/medium/low): ").lower()
-time_bound = input("Is it time bound? (yes/no):").lower()
+time-bound = input("Is it time bound? (yes/no):").lower()
 
 match priority:
     case "high":
         level = "high"
-        if time_bound == "yes":
+        if time-bound == "yes":
             print(f"Reminder: { task} is a {level} priority task that requires immediate attention today!" )
         else:
             print(f"Note: { task} is a {level} priority task. Consider completing it when you have free time.")
     case "medium":
         level = "medium"
-        if time_bound == "yes":
+        if time-bound == "yes":
             print(f"Reminder: { task} is a {level} priority task that requires immediate attention today!" )
         else:
              print(f"Note: { task} is a {level} priority task. Consider completing it when you have free time.")
     case "low":
         level = "low"
-        if time_bound == "yes":
+        if time-bound == "yes":
             print(f"Reminder: { task} is a {level} priority task that requires immediate attention today!" )
         else:
             print(f"Note: { task} is a {level} priority task. Consider completing it when you have free time.")
