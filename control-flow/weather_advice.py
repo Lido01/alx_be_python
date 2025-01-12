e046ca76f2876323f6949dbaf16ca6d35d6d3f9e
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-11 16:08:36 -0800

    Update weather_advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,9 @@
-weather_input=input("What's the weather like today?(sunny/rainy/cold):")
-
-if(weather_input=="sunny"):
+weather_input = input("What's the weather like today?(sunny/rainy/cold):").lower()
+if(weather_input == "sunny"):
     print("Wear a t-shirt and sunglasses")
-elif(weather_input=="rainy"):
+elif(weather_input == "rainy"):
     print("Don't forget your umbrella and a raincoat")
-elif(weather_input=="cold"):
+elif(weather_input == "cold"):
     print("Make sure to wear a warm coat and a scarf")
 else:
     print("Sorry, I don't have recommentrions for this weather")
