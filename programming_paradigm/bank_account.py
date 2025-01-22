dfe97469117de34d6ded06031ac4682b84a4f544
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-22 03:04:53 -0800

    unwanted space and undentaiton

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,11 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self._account_balance = initial_balance
-
     def deposit(self, amount):
         if amount > 0:
             self._account_balance += amount
         else:
             print("Deposit amount must be positive.")
-
     def withdraw(self, amount):
         if amount > 0 and amount <= self._account_balance:
             self._account_balance -= amount
@@ -18,7 +16,5 @@ class BankAccount:
         else:
             print("Withdrawal amount must be positive.")
             return False
-
     def display_balance(self):
         print(f"Current balance: ${self._account_balance:.2f}")
-
