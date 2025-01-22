af868ee575815d7d7beca74fa3f58715c8aede4e
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-01-22 03:00:10 -0800

    spliotinig the main and project part

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,3 +1,24 @@
-# Banking system
-class Banck_system():
-    
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            return True
+        elif amount > self._account_balance:
+            print("Insufficient funds.")
+            return False
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self._account_balance:.2f}")
+
