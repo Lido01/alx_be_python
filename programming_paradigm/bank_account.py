fb4dbeb7e4dea1690109ec55e0bc08a13b5914a9
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-23 10:54:06 -0800

    correcting the current balance display output

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,4 +17,4 @@ class BankAccount:
             print("Withdrawal amount must be positive.")
             return False
     def display_balance(self):
-        print(f"Current balance: ${self._account_balance:.2f}")
+        print(f"Current Balance: ${self._account_balance:.2f}")
