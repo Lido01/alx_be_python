71decc9c47a75eb52a72a143bf4ef6b7071897fb
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-26 14:16:56 -0800

    adding each file main(test code) as comment explain

--- oop/main.py
+++ oop/main.py
@@ -1,6 +1,6 @@
+#1 This main part is for book_class file
 from book_class import Book
-
-def maindef main():
+def main():
     # Creating an instance of Book
     my_book = Book("1984", "George Orwell", 1949)
 
@@ -15,3 +15,26 @@ def maindef main():
 
 if __name__ == "__main__":
     main()
+
+#2 This part is for library_system
+"""from library_system import Book, EBook, PrintBook, Library
+
+def main():
+    # Create a Library instance
+    my_library = Library()
+
+    # Create instances of each type of book
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+    # Add books to the library
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    # List all books in the library
+    my_library.list_books()
+
+if __name__ == "__main__":
+    main()"""
