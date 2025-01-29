c2063ca0425bb25d54a861f585876f6a49fced71
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-29 03:39:31 -0800

    adding static method and class method

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,17 @@
+# Static method and class method
+class Calculator:
+    calculation_type = f"Arithiemtic Operations"
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    # def __str__(self):
+    #     return f" The Sum is: {self.a + self.b} "
+    @staticmethod
+    def add(a, b):
+        return a + b
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+    
