1302730d781e43a4b451d3b4b84d5ba0edcf23c1
Lidetu Tesfaye <lidosinafanis@gmail.com>
2025-01-29 03:49:11 -0800

    destroying unwanted code part

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,15 +1,11 @@
 # Static method and class method
 class Calculator:
     calculation_type = "Arithiemtic Operations"
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    # def __str__(self):
-    #     return f" The Sum is: {self.a + self.b} "
+    
     @staticmethod
     def add(a, b):
         return a + b
+        
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
